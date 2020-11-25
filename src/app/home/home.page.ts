8f4dc2378ad0944b06ccb56cf0e5356857512e8d
YogehswariAllapure <yallapure@gmail.com>
2020-11-25 11:52:23 +0530

    google login on ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import '@codetrix-studio/capacitor-google-auth';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  userInfo = null;
 
   constructor() {}
+
+
+  async googleSignup() {
+    const googleUser = await Plugins.GoogleAuth.signIn(null) as any;
+    console.log('my user: ', googleUser);
+    this.userInfo = googleUser;
+    
+  }
 
 }
